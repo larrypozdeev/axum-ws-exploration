b165dd920cf4d8610c704985c1d505d926382a00
Larry Pozdeev <larrypozdeev@gmail.com>
2024-02-06 22:51:39 -0800

    Adds chatting. Backend

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use futures::StreamExt;
-use std::{collections::HashSet, sync::Arc};
-
 use axum::{
     extract::{
-        ws::{WebSocket, WebSocketUpgrade, Message},
+        ws::{Message, WebSocket, WebSocketUpgrade},
         State,
     },
     response::{Html, Response},
     routing::get,
     Router,
 };
+use futures::SinkExt;
+use futures::StreamExt;
+use std::{collections::HashSet, sync::Arc};
+use tokio::sync::broadcast;
 use tokio::sync::Mutex;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 struct AppState {
     user_set: Mutex<HashSet<String>>,
+    tx: broadcast::Sender<String>,
 }
 
 #[tokio::main]
 async fn main() {
     let user_set = Mutex::new(HashSet::new());
-    let state = Arc::new(AppState { user_set });
+    let (tx, _rx) = broadcast::channel(100);
+
+    let state = Arc::new(AppState { user_set, tx });
 
     let app = Router::new()
         .route("/chat", get(chat_handler))
@@ -37,15 +42,76 @@ async fn websocket_handler(ws: WebSocketUpgrade, State(state): State<Arc<AppStat
     ws.on_upgrade(|socket| websocket(socket, state))
 }
 
-async fn websocket(mut socket: WebSocket, state: Arc<AppState>) {
+async fn websocket(socket: WebSocket, state: Arc<AppState>) {
     let (mut sender, mut receiver) = socket.split();
 
-    let username = String::new();
+    let mut username = String::new();
     while let Some(Ok(message)) = receiver.next().await {
-       // print out the message
-        println!("{:?}", message);
+        if let Message::Text(name) = message {
+            let mut user_set = state.user_set.lock().await;
 
+            if !user_set.contains(&name) {
+                user_set.insert(name.to_owned());
+
+                username.push_str(&name);
+            }
+
+            if !username.is_empty() {
+                break;
+            } else {
+                // Only send our client that username is taken.
+                let _ = sender
+                    .send(Message::Text(String::from("Username already taken.")))
+                    .await;
+
+                return;
+            }
+        };
     }
+
+    let mut rx = state.tx.subscribe();
+
+    let msg = format!("{username} joined.");
+    tracing::debug!("{msg}");
+    // works, but doesn't keep the connection alive. broadcast is required
+    // sender.send(Message::Text(msg.clone())).await.unwrap();
+    state.tx.send(msg).unwrap();
+
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            // In any websocket error, break loop.
+            if sender.send(Message::Text(msg)).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    // first part is completed
+
+    // cloning things for receiving side
+
+    let tx = state.tx.clone();
+    let name = username.clone();
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(text))) = receiver.next().await {
+            // Add username before message.
+            let _ = tx.send(format!("{name}: {text}"));
+        }
+    });
+
+    tokio::select! {
+        _ = (&mut send_task) => recv_task.abort(),
+        _ = (&mut recv_task) => send_task.abort(),
+    }
+
+    // Send "user left" message (similar to "joined" above).
+    let msg = format!("{username} left.");
+    tracing::debug!("{msg}");
+    let _ = state.tx.send(msg);
+
+    // Remove username from map so new clients can take it again.
+    state.user_set.lock().await.remove(&username);
 }
 
 async fn chat_handler() -> Html<&'static str> {
